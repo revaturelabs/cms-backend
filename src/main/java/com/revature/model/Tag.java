0424316d45ee0782749ba696da0592fc4f5a37e2
John Iversen <jiversen.tu@gmail.com>
2019-05-21 16:16:08 -0400

    added applicationContext.xml with h2 info

--- src/main/java/com/revature/model/Tag.java
+++ src/main/java/com/revature/model/Tag.java
@@ -1,5 +1,110 @@
 package com.revature.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TAG")
 public class Tag {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="ailmentSequence")
+	@SequenceGenerator(name="ailmentSequence",sequenceName="AILMENT_SEQ", allocationSize=1)
+	@Column(name="TAG_ID")
+	private long tagId;
+	@Column(name = "TAG_NAME")
+	private String name;
+	@Column(name = "TYPE")
+	private String type;
+	@Column(name = "CONTENT_ID")
+	private long contentId;
+	@Column(name = "MODULE_ID")
+	private long moduleId;
+	@Column(name = "DATE_CREATED")
+	private String dateCreated;
+	@Column(name = "DATE_UPDATED")
+	private String dateUpdated;
+
+	
+	public Tag() {}
+	
+	public Tag(long tagId, String name, String type, long contentId, long moduleId, String created, String updated) {
+		super();
+		this.tagId = tagId;
+		this.name = name;
+		this.type = type;
+		this.contentId = contentId;
+		this.moduleId = moduleId;
+		this.dateCreated = created;
+		this.dateUpdated = updated;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Tag [tagId=" + tagId + ", name=" + name + ", type=" + type + ", contentId=" + contentId + ", moduleId="
+				+ moduleId + ", dateCreated=" + dateCreated + ", dateUpdated=" + dateUpdated + "]";
+	}
+
+	public long getTagId() {
+		return tagId;
+	}
+
+	public void setTagId(long tagId) {
+		this.tagId = tagId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(Long contentId) {
+		this.contentId = contentId;
+	}
+
+	public long getModuleId() {
+		return moduleId;
+	}
+
+	public void setModuleId(long moduleId) {
+		this.moduleId = moduleId;
+	}
+
+	public String getCreated() {
+		return dateCreated;
+	}
+
+	public void setCreated(String created) {
+		this.dateCreated = created;
+	}
+
+	public String getUpdated() {
+		return dateUpdated;
+	}
+
+	public void setUpdated(String updated) {
+		this.dateUpdated = updated;
+	}
 
 }
