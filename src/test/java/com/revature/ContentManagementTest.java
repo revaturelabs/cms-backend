38a7d19d615cfbcb2c9d3b54996dd658a38f6458
matt <mattcowin82@gmail.com>
2019-05-21 13:47:56 -0400

    Needing to pull the updated Repository and Service

--- src/test/java/com/revature/ContentManagementTest.java
+++ src/test/java/com/revature/ContentManagementTest.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.model.Content;
 import com.revature.repository.ContentRepository;
 import com.revature.service.ContentService;
 
@@ -24,7 +27,42 @@ public class ContentManagementTest {
 	private ContentService contentService;
 	
 	@Test
+	public void create_createTest() {
+		
+	}
+	
+	@Test
 	public void findByCategory_findByCategoryTest() {
+		
+	}
+	
+	@Test
+	public void findByTag_findByTagTest() {
+		
+	}
+	
+	@Test
+	public void findByTagAndCategory_findByTagAndCategoryTest() {
+		
+	}
+	
+	@Test
+	public void findByUrl_findByUrlTest() {
+//		Content url = new Content();
+//		entityManager.persist(url);
+//		entityManager.flush();
+//		
+//		Content found = contentRepository.findByURL(url.getUrl());
+//		    assertThat(found.getName(), is(equalTo()));
+	}
+	
+	@Test 
+	public void delete_deleteTest() {
+		
+	}
+	
+	@Test
+	public void update_updateTest() {
 		
 	}
 	
