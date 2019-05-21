26e9e40f1d2e27899eeba89b2a4eb7d4d5f4c376
matt <mattcowin82@gmail.com>
2019-05-21 12:53:39 -0400

    switching to model to add updates

---
+++ src/test/java/com/revature/ContentManagementTest.java
@@ -0,0 +1,31 @@
+package com.revature;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.repository.ContentRepository;
+import com.revature.service.ContentService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ContentManagementTest {
+	
+	@Autowired
+	private ContentRepository contentRepository;
+	
+	@Autowired
+    private TestEntityManager entityManager;
+	
+	@Autowired 
+	private ContentService contentService;
+	
+	@Test
+	public void findByTag_findByTest() {
+		
+	}
+	
+}
