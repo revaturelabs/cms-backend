eac766437a4e710047e60e2a1c4ea258d11243cd
Alexandra Briggs <alex.m.briggs@gmail.com>
2019-05-22 12:55:35 -0400

    updated pom.xml to match base branch and updated Module Test

--- src/test/java/com/revature/ModuleTest.java
+++ src/test/java/com/revature/ModuleTest.java
@@ -1,21 +1,16 @@
 package com.revature;
 
 import static org.assertj.core.api.Assertions.assertThat;
-<<<<<<< HEAD
-=======
 import static org.junit.Assert.assertEquals;
->>>>>>> moduleBranch.Alex
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-<<<<<<< HEAD
-=======
 import org.springframework.test.annotation.Rollback;
->>>>>>> moduleBranch.Alex
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +20,7 @@ import com.revature.repository.ModuleRepository;
 @RunWith(SpringRunner.class)
 @Transactional
 @DataJpaTest
-<<<<<<< HEAD
-=======
 @Rollback(true)
->>>>>>> moduleBranch.Alex
 public class ModuleTest {
 
 	@Autowired
@@ -40,7 +32,6 @@ public class ModuleTest {
 		module = newModule(module);
 		Module entry = moduleRepo.save(module);
 		assertThat(module).isEqualTo(entry);
-<<<<<<< HEAD
 	}
 
 	private Module newModule(Module module) {
@@ -52,36 +43,9 @@ public class ModuleTest {
 		module.setUpdated(new Date());
 		return module;
 	}
-	/*
-	 * @Test public void testFindAllModulesShouldReturnAll() { List<Module> modules
-	 * = moduleRepo.findAll(); assertEquals(1, modules.size()); }
-	 * 
-	 * @Test public void testFindModuleByName() { Module module =
-	 * moduleRepo.findModuleByName("moduleName"); assertEquals("moduleName",
-	 * module.getModuleName()); }
-	 * 
-	 * @Test public void testUpdateModule() { Module module = new Module(0,
-	 * "module1", true, null, new Date()); module.setModuleName("newModuleName");
-	 * moduleRepo.save(module); assertEquals("newModuleName",
-	 * module.getModuleName()); }
-	 * 
-	 * @Test public void testDeleteModule() { Module module = new Module(0,
-	 * "module2", true, new Date(), new Date()); moduleRepo.delete(module);
-	 * List<Module> modules = moduleRepo.findAll(); assertEquals(0, modules.size());
-	 * }
-	 */
-=======
-	}
 
-	private Module newModule(Module module) {
-		module = new Module();
-		module.setModuleId(1);
-		module.setModuleName("Module 1");
-		module.setActive(false);
-		module.setCreated(new Date());
-		module.setUpdated(new Date());
-		return module;
-	}
+
+
 
 	@Test
 	public void testFindAllModulesShouldReturnAll() {
@@ -126,6 +90,5 @@ public class ModuleTest {
 		List<Module> modules = moduleRepo.findAll();
 		assertEquals(0, modules.size());
 	}
->>>>>>> moduleBranch.Alex
 
 }
