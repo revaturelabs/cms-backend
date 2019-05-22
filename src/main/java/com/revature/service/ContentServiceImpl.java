15adae598d46962b49adfaaf8bd69f39c9e4aa04
Jeffafa1992 <nicholasrocksvold@yahoo.com>
2019-05-22 15:19:05 -0400

    minor adjustments

--- src/main/java/com/revature/service/ContentServiceImpl.java
+++ src/main/java/com/revature/service/ContentServiceImpl.java
@@ -54,11 +54,12 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Override
 	public Content findByURL(String url) {
-		try {
-			throw new RuntimeException("url: ssssssss");
-		} catch(UrlNotRecognizedException e) {
-			e.printStackTrace();
-		}
+		//only used for testing
+//		try {
+//			throw new RuntimeException("url: ssssssss");
+//		} catch(UrlNotRecognizedException e) {
+//			e.printStackTrace();
+//		}
 		return contentRepository.findByUrl(url);
 	}
 	
