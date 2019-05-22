712d69a3d9445760f7c13df69a91ebaed0a1cf13
Tech314 <kyle.magdales@yahoo.com>
2019-05-22 14:48:48 -0400

    Added CrossOrigin and RequestMapping annotations to TagController

--- src/main/java/com/revature/controller/TagControllerImpl.java
+++ src/main/java/com/revature/controller/TagControllerImpl.java
@@ -3,17 +3,22 @@ package com.revature.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Tag;
 import com.revature.service.TagServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
 public class TagControllerImpl implements TagController {
 	
 	@Autowired
