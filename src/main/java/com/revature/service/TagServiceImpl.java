6a5ac45ea6964b06fbd8e680ffcd434ff593935f
John Iversen <jiversen.tu@gmail.com>
2019-05-23 13:12:36 -0400

    merged base to contentDoc

--- src/main/java/com/revature/service/TagServiceImpl.java
+++ src/main/java/com/revature/service/TagServiceImpl.java
@@ -8,36 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.model.Tag;
-//import com.revature.repository.TagRepository;
+import com.revature.repository.TagRepository;
 
 @Service
 public class TagServiceImpl implements TagService {
 	
 	// Dependency
 	@Autowired
-	//TagRepository tagRepository;
-	
-		
-	//private static Logger logger= Logger.getLogger();
+	TagRepository tagRepository;
 	
 	public List<Tag> getAllTags() {
 		List<Tag> tags = new ArrayList<Tag>();
-		//tagRepository.findAll().forEach(tag -> tags.add(tag));
+		tagRepository.findAll().forEach(tag -> tags.add(tag));
 		return tags;
 	}
 		
 	public Tag getTagById(long id) {
-        //return tagRepository.findById(id).get();
-		return null;
+        return tagRepository.findById(id).get();
     }
 
     public void saveOrUpdate(Tag tag) {
     	tag.setUpdated(new Date());
-        //tagRepository.save(tag);
+        tagRepository.save(tag);
     }
 
+    /**
+     * Delete Tag By Id Method:
+     * 1. Receives long value of id.
+     */
     public void delete(long id) {
-        //tagRepository.deleteById(id);
+        tagRepository.deleteById(id);
     }
-
 }
