d7b9e7cc485d055fea6636f5ae7a189c0dc9739e
宮崎啓介 <kmiyazaki56@gmail.com>
2019-05-23 21:12:59 -0400

    Tag branch (#13)
    * End of day push 5/21/19
    
    * Updated Tag create and update
    
    * Updated Tags create and update
    
    * Revert "Updated Tags create and update"
    
    This reverts commit 42af4736ab8ac94a948387e3ba60dd1ab2d1d6e6.
    
    * Updated versions of Tag Controller, Service, Repository
    
    * Fixed files
    
    * Renamed to tagName
    
    * fixed conflicts and bad methods calls
    
    * added setDateUpdated call to TagServiceImpl
    
    * Changed mapping for create and update methods

--- src/main/java/com/revature/service/TagServiceImpl.java
+++ src/main/java/com/revature/service/TagServiceImpl.java
@@ -7,8 +7,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.exception.ModuleNotFoundExceptions;
+import com.revature.exception.TagNotFoundExceptions;
+import com.revature.model.Module;
 import com.revature.model.Tag;
 import com.revature.repository.TagRepository;
+
 
 @Service
 public class TagServiceImpl implements TagService {
@@ -20,24 +24,41 @@ public class TagServiceImpl implements TagService {
 		
 	//private static Logger logger= Logger.getLogger();
 	
-	public List<Tag> getAllTags() {
+	public List<Tag> findAllTags() {
 		List<Tag> tags = new ArrayList<Tag>();
 		tagRepository.findAll().forEach(tag -> tags.add(tag));
 		return tags;
 	}
 		
-	public Tag getTagById(long id) {
-        return tagRepository.findById(id).get();
-		
+	public Tag findTagById(long id) {
+        //return tagRepository.findById(id).get();
+		return null;
     }
+	
+	@Override
+	public Tag findByTagName(String name) {
+		try {
+			return tagRepository.findByTagName(name);
+		} catch (TagNotFoundExceptions e) {
+			throw new TagNotFoundExceptions(name);
+		}
+	}
 
-    public void saveOrUpdate(Tag tag) {
-    	tag.setUpdated(new Date());
+    public void save(Tag tag) {
+    	tag.setDateCreated(new Date());
         tagRepository.save(tag);
     }
 
-    public void delete(long id) {
-        tagRepository.deleteById(id);
+    @Override
+    public void deleteTag(Tag tag) {
+        tagRepository.delete(tag);
     }
+    
+    public void update(Tag tag) {
+    	tag.setDateUpdated(new Date());
+        tagRepository.save(tag);
+    }
+
+
 
 }
