1bb545fd5659265467a6e0757f0643716373c91f
Tech314 <kyle.magdales@yahoo.com>
2019-05-28 09:50:51 -0400

    Removed tomcat plugin and SpringServletInitializer

--- src/main/java/com/revature/CmsForceApplication.java
+++ src/main/java/com/revature/CmsForceApplication.java
@@ -2,16 +2,9 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CmsForceApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CmsForceApplication.class);
-	}
+public class CmsForceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CmsForceApplication.class, args);
